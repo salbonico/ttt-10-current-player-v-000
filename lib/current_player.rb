558fca294777d08893a606756729ae537d9b08ec
Steve Albonico <salbonico@gmail.com>
2019-01-12 00:28:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,19 @@
-def current_player(board)
-counter = 0
-board.each do |spot|
-if spot != " "
-  counter +=1
-end
-end
 
-if counter % 2 == 0
+def turn_count(board)
+  counter = 0
+  board.each do |spot|
+  if spot != " "
+    counter +=1
+  end
+  end
+  return counter
+end  
+
+
+
+def current_player(board)
+player = turn_count(board)
+if player % 2 == 0
   return "X"
 else return "O"
 end
